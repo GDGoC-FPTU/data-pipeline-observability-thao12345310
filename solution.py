344256aa3e74c190d14dab5172f12e6b4a3236e0
Dương Phương Thảo <duongphuongthao08102004@gmail.com>
2026-04-15 14:52:37 +0700

    feat: hoan thanh bai lab ngay 10

--- solution.py
+++ solution.py
@@ -2,8 +2,8 @@
 ==============================================================
 Day 10 Lab: Build Your First Automated ETL Pipeline
 ==============================================================
-Student ID: AI20K-XXXX  (<-- Thay XXXX bang ma so cua ban)
-Name: Your Name Here
+Student ID: 2A202600049  (<-- Thay XXXX bang ma so cua ban)
+Name: Dương Phương Thảo
 
 Nhiem vu:
    1. Extract:   Doc du lieu tu file JSON
